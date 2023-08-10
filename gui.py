014878d7766d71eb3439e5190d13597519c557d7
maykowsm <mayKow.menezes@projectivatecnologia.com.br>
2023-08-09 21:28:04 -0300

    update gui

--- gui.py
+++ gui.py
@@ -10,13 +10,21 @@ height = 700
 fps = 60
 
 
-#Consfigurações de inicialização
+# Consfigurações de inicialização
 window_surface= pygame.display.set_mode((width, height), flags= pygame.NOFRAME )
-pygame.display.set_caption("Exemplo Pygame com Botões")
-
+pygame.display.set_caption("BrCAD")
 clk = pygame.time.Clock()
 
 while True:
-   
+
    clk.tick(fps)
+
+
+class Menu:
+   def __init__(self) -> None:
+      pass
+
+class Button:
+   def __init__(self) -> None:
+      pass
 
